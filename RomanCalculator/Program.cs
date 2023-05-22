542148d54458145c254c4aa66b7440410ad9e0ce
Konstantin Pohilko <131478030+kontastinn@users.noreply.github.com>
2023-05-23 00:35:24 +0300

    Update Program.cs
    Just  replaced some loops in methods to dictionaries

--- RomanCalculator/Program.cs
+++ RomanCalculator/Program.cs
@@ -1,153 +1,146 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace RomanCalculator
 {
     internal class Program
     {
-        //
-        //using global variables to working with them in any method
-        //
-        static int[] Data = { 0, 0 };
-        static int indexData = 0;
-        static int dataResult;
-        static int curr = 0;
-        static string result;
-        
-        static void RomanToNumber()
-        {           
-            //
-            //This method calls twice and check your input, divide your string to pieces like "CD",
-            //If  method  cannot find a similar character, it will look for a character such as "C"
-            //after for every character checking this method translate your string value into int variable and will store it
-            //
-            string s = Console.ReadLine();
+        private static int[] Data = new int[2];
+        private static int indexData;
+        private static int dataResult;
 
-            int[] num = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
-            string[] rom = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
-            int index = 0;
+        /// <summary>
+        /// Calculating two Roman values
+        /// </summary>
+        static void RomanCalulate()
+        {
+            int answer = 1;
 
-            for (int i = 0; i < rom.Length && index < s.Length; i++)
+            while (answer == 1)
             {
-                for (int d = 0; d < rom.Length && index + 1 < s.Length; d++)
+                indexData = 0;
+                Data[0] = 0;
+                Data[1] = 0;
+                Console.Clear();
+                try
                 {
-                    string ch1 = s[index] + s[index + 1].ToString();
-                    if (ch1 == rom[d])
+                    Console.Write("Enter first Roman value: ");
+                    RomanToInt();
+                    indexData++;
+                    Console.Write("\nEnter second Roman value: ");
+                    RomanToInt();
+                    Console.WriteLine("\nChoose math operaton:\n1 - plus;\n2 - minus;\n1 - multiply;\n1 - divide;\n");
+                    Console.Write("Enter here: ");
+                    string answer1 = Console.ReadLine();
+                    if (answer1 == "1")
+                        dataResult = Data[0] + Data[1];
+                    else if (answer1 == "2")
+                        dataResult = Data[0] - Data[1];
+                    else if (answer1 == "3")
+                        dataResult = Data[0] * Data[1];
+                    else if (answer1 == "4")
+                        dataResult = Data[0] / Data[1];
+                    else
                     {
-                        Data[indexData] += num[d];
-                        index++; index++;
-                        d = -1;
+                        Console.WriteLine("You chose wrong option");
+                        Console.ReadLine();
+                        Console.Clear();
+                        return;
                     }
+                    Console.Write("\nYour result is : ");
+                    IntToRoman();
+                    Console.WriteLine("\n\nIf you wanna try again choose option:\n 1 - yes, i want;\n 2 - that is enough, baby ");
+                    answer = int.Parse(Console.ReadLine());
                 }
-
-                if (index != s.Length)
+                catch
                 {
-                    string ch = s[index].ToString();
-                    if (ch == rom[i])
-                    {
-                        Data[indexData] += num[i];
-                        index++;
-                        i = -1;
-                    }
+                    Console.WriteLine("You entered wrong value, try again");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Converting int to Roman value
+        /// </summary>
+        /// <param name="value"></param>
+        static void IntToRoman()
+        {
+            string romanResult = string.Empty;
+            Dictionary<string, int> romanNumbersDictionary = new()
+            {
+                {"I", 1  },
+                {"IV",4  },
+                {"V", 5  },
+                {"IX",9  },
+                {"X", 10  },
+                {"XL",40  },
+                {"L", 50  },
+                {"XC",90  },
+                {"C", 100 },
+                {"CD",400 },
+                {"D", 500 },
+                {"CM",900 },
+                {"M", 1000}
+            };
+            if (dataResult == 0)
+            {
+                romanResult = "0";
+            }
+            foreach (var item in romanNumbersDictionary.Reverse())
+            {
+                while (dataResult > 0 && dataResult >= item.Value)
+                {
+                    dataResult -= item.Value;
+                    romanResult += item.Key;
+                }
+            }
+            Console.Write(romanResult);
+        }
+
+        /// <summary>
+        /// Converting Roman value to int
+        /// </summary>
+        /// <param name="romanValue"></param>
+        /// <returns></returns>
+        static void RomanToInt()
+        {
+            string romanValue = Console.ReadLine();
+            Dictionary<char, int> romanNumbersDictionary = new()
+            {
+                {'I', 1  },
+                {'V', 5  },
+                {'X', 10  },
+                {'L', 50  },
+                {'C', 100 },
+                {'D', 500 },
+                {'M', 1000}
+            };
+            for (int i = 0; i < romanValue.Length; i++)
+            {
+                char currentRomanChar = romanValue[i];
+                romanNumbersDictionary.TryGetValue(currentRomanChar, out int currentNumber);
+
+                if (i + 1 < romanValue.Length && romanNumbersDictionary[romanValue[i + 1]] > romanNumbersDictionary[currentRomanChar])
+                {
+                    Data[indexData] -= currentNumber;
+                }
+                else
+                {
+                    Data[indexData] += currentNumber;
                 }
             }
         }
-        //
-        //this math method operate your two converted strings to int numbers
-        //after the chosed one of four operations your result will be stored in your new int variable
-        //
-        static void Math()
-        {
-            Console.WriteLine("\nChoose an operation:\n1 - Plus\n2 - Minus\n3 - Multiply\n4 - Divide");
-            Console.Write("\nEnter: ");
-            string answer = Console.ReadLine();
-            Console.WriteLine();
-            if (answer == "1")
-                dataResult = Data[0] + Data[1];
-            else if (answer == "2")
-                dataResult = Data[0] - Data[1];
-            else if (answer == "3")
-                dataResult = Data[0] * Data[1];
-            else if (answer == "4")
-                dataResult = Data[0] / Data[1];
-            else
-            {
-                Console.ReadLine();
-                return;
-            }
-            curr++;
-            //the last method for stored int variable
-            NumberToRoman(dataResult);
-        }
-        //
-        // the last method will convert your int result which is stored in your new int variable, to a string result
-        //
-        static void NumberToRoman(int value)
-        {
-            string[] units = { " ", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
-            string[] dozens = { "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
-            string[] hundreds = { "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
-            string[] thousands = { "M", "MM", "MMM" };
-
-            //int value and the result  must be between between 1 and 3999
-            // if you will be outside of these values the method will return nothing;
-            if (value >= 1000 && value < 3999)
-            {
-                int Thousands = value / 1000; value %= 1000;
-                string result = thousands[Thousands - 1];
-                if (curr == 1) Console.Write(result);
-            }
-
-            if (value >= 100 && value < 1000)
-            {
-                int Hundreds = value / 100; value %= 100;
-                string result = hundreds[Hundreds - 1];
-                if (curr == 1) Console.Write(result);
-            }
-
-            if (value >= 10 && value < 100)
-            {
-                int Dozens = value / 10; value %= 10;
-                string result = dozens[Dozens - 1];
-                if (curr == 1) Console.Write(result);
-            }
-
-            if (value > 0 && value < 10)
-            {
-                string result = units[value];
-                if (curr == 1) Console.Write(result);
-            }
-        }
-
-        //
-        //this method is the final program for calling it in Main, just summing all of these methods into one big method
-        //
-        static void RomanCalculator()
-        {
-                Console.Write("Enter first Roman value: ");
-                RomanToNumber();
-                indexData++;
-                Console.WriteLine();
-                Console.Write("Enter second Roman value: ");
-                RomanToNumber();
-                Math();
-                Console.ReadLine();           
-        }
-
         static void Main()
         {
-            //calling method
-            RomanCalculator();
-            Console.WriteLine();
+            RomanCalulate();
         }
-
-
-        
-
     }
-
-
 }
